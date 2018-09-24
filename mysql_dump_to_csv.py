6d211389b0b1e1017f204a4ff7f6c7f4df5b72a8
cadsdeveloper <data@c4ads.org>
2018-09-24 11:36:06 -0400

    fixed multiple inserts overwriting; now stores all correctly

--- mysql_dump_to_csv.py
+++ mysql_dump_to_csv.py
@@ -5,6 +5,7 @@ import sys
 import re
 
 SCHEMAS = {}
+VALUES = {}
 
 
 def is_create_statement(line):
@@ -46,7 +47,6 @@ def write_file(output_directory, table_name, schema, values):
 
 def parse_file(file_name, output_directory):
     current_table_name = None
-
     with open(file_name, 'r') as read_file:
         for line in read_file:
             if is_create_statement(line):
@@ -59,7 +59,17 @@ def parse_file(file_name, output_directory):
                 current_table_name = get_mysql_name_value(line)
                 current_schema = SCHEMAS[current_table_name]
                 values = get_value_tuples(line)
-                write_file(output_directory, current_table_name, current_schema, values)
 
+                # append these values to the table's existing ones, if any exist
+                if not current_table_name in VALUES:
+                    VALUES[current_table_name] = []
+                for value in values:
+                    VALUES[current_table_name].append(value)
+
+        # we've stored the entire set of schemas in SCHEMAS and the entire set
+        # of data in VALUES, both indexed by table name -- now write them
+        for tableName,schema in SCHEMAS.items():
+            write_file(output_directory, tableName, schema, VALUES[tableName])
 if __name__ == '__main__':
     parse_file(sys.argv[1], sys.argv[2])
+
